446e2bcd929ae4895e14642444a4c8020183b4c0
fernando <fdbarroso@gmail.com>
2023-10-18 22:40:04 -0300

    mejoras a la entidad estado}

--- src/main/java/imb/pr3/delivery/controller/EstadoController.java
+++ src/main/java/imb/pr3/delivery/controller/EstadoController.java
@@ -1,11 +1,12 @@
 package imb.pr3.delivery.controller;
 
-
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.tomcat.util.http.ResponseUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,57 +19,55 @@ import org.springframework.web.bind.annotation.RestController;
 
 import imb.pr3.delivery.entity.Estado;
 import imb.pr3.delivery.service.IEstadoService;
+import imb.pr3.delivery.util.ResponseUtil;
 import jakarta.validation.ConstraintViolationException;
-
 
 @RestController
 @RequestMapping(path = "api/estado")
 public class EstadoController {
-	
-	
-	private IEstadoService EstadoService;
-	
-	public EstadoController(IEstadoService estadoService) {
-		this.EstadoService = estadoService;
-	}
+	@Autowired
+	IEstadoService estadoService;
 
+	// buscarTodos
 	@GetMapping("")
-	public ResponseEntity<ApiResponse<List<Estado>>> obtenerTodosEstados() {
-	    List<Estado> estados = EstadoService.findAll();
-	    return estados.isEmpty() ? ResponseUtil.notFound("No se encontraron datos para Cliente") : ResponseUtil.success(estados));
+	public ResponseEntity<APIResponse<List<Estado>>> obtenerTodosLosClientes() throws Exception {
+		List<Estado> estado = estadoService.findAll();
+		return estado.isEmpty() ? ResponseUtil.notFound("No hay estados disponibles")
+		           : ResponseUtil.success(estado); 
 	}
-
+	@GetMapping("{id}")
+	public ResponseEntity<APIResponse<Optional<Estado>>> buscarEstadoPorId(@PathVariable("id") Integer id){
+		Optional<Estado> estado=  estadoService.findById(id);
+		return estado == null ? ResponseUtil.notFound("No se encontró el estado con el identificador proporcionado")
+				: ResponseUtil.success(estado);	
+	}
+	@PostMapping
+    public ResponseEntity<APIResponse<Estado>> crearEstado(@RequestBody Estado estado, BindingResult result) {
+        return estadoService.existe(estado.getId()) ? ResponseUtil.badRequest("No se puede crear un estado, el ID ingresado ya existe")
+        		: ResponseUtil.success(estado);
+     }
+	@PutMapping
+	public ResponseEntity<APIResponse<Estado>> modificarEstado(@RequestBody Estado estado) {
+		return estadoService.existe(estado.getId()) ? ResponseUtil.success(estadoService.save(estado))
+				: ResponseUtil.badRequest("No se puede actualizar estado, el ID ingresado no ha sido creado");
+	}
+	@DeleteMapping("{id}")
+	public ResponseEntity<APIResponse<Estado>> eliminarEstado(@PathVariable("id") Integer id) {
+		 return estadoService.existe(id) ? ResponseUtil.success(null):
+			 ResponseUtil.badRequest("No se puede actualizar estado, el ID ingresado no ha sido creado");			 		
+	}
+	// Excepciones
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<APIResponse<Estado>> handleException(Exception ex) {    	
+    	return ResponseUtil.badRequest(ex.getMessage());
+    }
+    
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<APIResponse<Estado>> handleConstraintViolationException(ConstraintViolationException ex) {
+    	return ResponseUtil.handleConstraintException(ex);
+    } 
+   
 	
-	@GetMapping("/{id}")
-	public ResponseEntity<ApiResponse<Optional<Estado>>> obtenerEstadoPorId(@PathVariable Integer id){
-		Optional<Estado> estado = EstadoService.findById(id);
-		return estado.isPresent() ? ResponseUtil.success(estado) : ResponseUtil.notFound("Cliente no encontrado");
-	}
 	
-	@PostMapping("")
-	public ResponseEntity<ApiResponse<Estado>> crearCliente(@RequestBody Estado estado){
-		return EstadoService.existe(estado.getId()) ? ResponseUtil.badRequest("Debe ingresar un nombre") : ResponseUtil.created(EstadoService.save(estado));
-	}
-	
-	@PutMapping("")
-	public ResponseEntity<ApiResponse<Estado>> modificarCliente(@RequestBody Estado estado){
-		return EstadoService.existe(estado.getId()) ?  ResponseUtil.success(EstadoService.save(estado)) 
-				: ResponseUtil.badRequest("No existe estado con el id especificado");
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<ApiResponse<Boolean>> eliminarEstado(@PathVariable Integer id) {
-		return EstadoService.existe(id) ? ResponseUtil.success(EstadoService.delete(id)) : ResponseUtil.badRequest("El ID no existe");
-	}
-	
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ApiResponse<Estado>> handleException(Exception ex) {    	
-	    return ResponseUtil.badRequest(ex.getMessage());
-	}
-	    
-	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<ApiResponse<Estado>> handleConstraintViolationException(ConstraintViolationException ex) {
-	    return ResponseUtil.handleConstraintException(ex);
-	}    
 
 }
